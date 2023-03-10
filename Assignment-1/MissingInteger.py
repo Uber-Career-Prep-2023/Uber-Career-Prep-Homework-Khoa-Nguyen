efb782bdd04b7ad364c489eff392adeb0f99f0c7
Khoa Nguyen <115177830+khoawack@users.noreply.github.com>
2023-03-09 23:22:53 -0800

    Add files via upload

---
+++ Assignment-1/MissingInteger.py
@@ -0,0 +1,24 @@
+#Two arrays two pointer
+
+def missing_integer(arr: list[int], n: int) -> int:
+    array2 = [i for i in range(1,n+1)]
+
+    for i in array2:
+        if i not in arr:
+            return i
+
+
+
+
+
+
+
+
+print(missing_integer([1, 2, 3, 4, 6, 7], 7)) # expected = 5
+
+print(missing_integer([1], 2)) # expected = 2
+
+print(missing_integer([1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12], 12)) # expected = 9
+
+# Time Complexity = O(n)
+# Space Complexity = O(n)
